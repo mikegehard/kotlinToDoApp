596bcd41e36d386429f78122fb1f46855a36cb52
Mike Gehard <mgehard@pivotal.io>
2016-03-24 13:28:56 -0600

    Some questions about the approach of hand wiring domain dependencies

--- src/main/kotlin/com/example/FooRepository.kt
+++ src/main/kotlin/com/example/FooRepository.kt
@@ -3,6 +3,12 @@ package com.example
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
+// Is there a way that I do not have to mark the repository
+// as a Spring @Component?
+// Would minimize the framework leaking into the domain layer?
+// Is a repository the domain layer?
+// Is it better to be explicit about wiring of domain dependencies
+// or just let the framework put them together?
 @Component
 class FooRepository {
     companion object {
